45f77639fadb43c8ca249e77e203050bd3f0ac91
Feichtmeier <frederik.feichtmeier@gmail.com>
2024-02-28 13:20:54 +0100

    hide titlebuttons :)

--- lib/home.dart
+++ lib/home.dart
@@ -18,6 +18,8 @@ class _HomeState extends State<Home> {
   XFile? file;
   late PhotoViewControllerBase<PhotoViewControllerValue> controller;
 
+  bool showButtons = false;
+
   @override
   void initState() {
     super.initState();
@@ -27,106 +29,116 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Scaffold(
-      body: Listener(
-        onPointerSignal: (event) {
-          if (controller.scale == null || event is! PointerScrollEvent) return;
+    return MouseRegion(
+      onEnter: (_) => setState(() => showButtons = true),
+      onExit: (_) => setState(() => showButtons = false),
+      child: Scaffold(
+        body: Listener(
+          onPointerSignal: (event) {
+            if (controller.scale == null || event is! PointerScrollEvent) {
+              return;
+            }
 
-          _zoom(event.scrollDelta.dy < 0);
-        },
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                child: file == null
-                    ? const SizedBox.shrink()
-                    : PhotoView(
-                        imageProvider: FileImage(File(file!.path)),
-                        minScale: PhotoViewComputedScale.contained * 0.8,
-                        maxScale: PhotoViewComputedScale.covered * 10,
-                        initialScale: PhotoViewComputedScale.contained * 0.5,
-                        controller: controller,
-                      ),
+            _zoom(event.scrollDelta.dy < 0);
+          },
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  child: file == null
+                      ? const SizedBox.shrink()
+                      : PhotoView(
+                          imageProvider: FileImage(File(file!.path)),
+                          minScale: PhotoViewComputedScale.contained * 0.8,
+                          maxScale: PhotoViewComputedScale.covered * 10,
+                          initialScale: PhotoViewComputedScale.contained * 0.5,
+                          controller: controller,
+                        ),
+                ),
               ),
-            ),
-            Container(
-              color: theme.scaffoldBackgroundColor,
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Center(
-                    child: SizedBox(
+              Container(
+                color: theme.scaffoldBackgroundColor,
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        height: 35,
+                        width: 35,
+                        child: YaruIconButton(
+                          onPressed: () => _zoom(false),
+                          icon: Center(
+                            child: Icon(
+                              YaruIcons.minus,
+                              color: theme.colorScheme.onSurface,
+                              size: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                          'Scale: ${controller.scale?.toStringAsFixed(2) ?? '0.5'}'),
+                    ),
+                    SizedBox(
                       height: 35,
                       width: 35,
                       child: YaruIconButton(
-                        onPressed: () => _zoom(false),
+                        onPressed: () => _zoom(true),
                         icon: Center(
                           child: Icon(
-                            YaruIcons.minus,
+                            YaruIcons.plus,
                             color: theme.colorScheme.onSurface,
                             size: 20,
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                        'Scale: ${controller.scale?.toStringAsFixed(2) ?? '0.5'}'),
-                  ),
-                  SizedBox(
-                    height: 35,
-                    width: 35,
-                    child: YaruIconButton(
-                      onPressed: () => _zoom(true),
-                      icon: Center(
-                        child: Icon(
-                          YaruIcons.plus,
-                          color: theme.colorScheme.onSurface,
-                          size: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      appBar: YaruWindowTitleBar(
-        titleSpacing: 0,
-        leading: Center(
-          child: SizedBox(
-            height: kYaruTitleBarItemHeight,
-            width: kYaruTitleBarItemHeight,
-            child: YaruIconButton(
-              onPressed: () async {
-                const XTypeGroup typeGroup = XTypeGroup(
-                  label: 'images',
-                  extensions: <String>['jpg', 'png'],
-                );
-                final newFile =
-                    await openFile(acceptedTypeGroups: <XTypeGroup>[typeGroup]);
+        appBar: YaruWindowTitleBar(
+          style: showButtons
+              ? YaruTitleBarStyle.normal
+              : YaruTitleBarStyle.undecorated,
+          // foregroundColor: showButtons ? null : Colors.transparent,
+          titleSpacing: 0,
+          leading: Center(
+            child: SizedBox(
+              height: kYaruTitleBarItemHeight,
+              width: kYaruTitleBarItemHeight,
+              child: YaruIconButton(
+                onPressed: () async {
+                  const XTypeGroup typeGroup = XTypeGroup(
+                    label: 'images',
+                    extensions: <String>['jpg', 'png'],
+                  );
+                  final newFile = await openFile(
+                      acceptedTypeGroups: <XTypeGroup>[typeGroup]);
 
-                setState(() {
-                  if (newFile != null) {
-                    file = newFile;
-                  }
-                });
-              },
-              icon: const Icon(
-                YaruIcons.image,
-                size: 20,
+                  setState(() {
+                    if (newFile != null) {
+                      file = newFile;
+                    }
+                  });
+                },
+                icon: const Icon(
+                  YaruIcons.image,
+                  size: 20,
+                ),
               ),
             ),
           ),
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          border: BorderSide.none,
+          title: Text(file?.path ?? 'Imaginator'),
         ),
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        border: BorderSide.none,
-        title: Text(file?.path ?? 'Imaginator'),
       ),
     );
   }
